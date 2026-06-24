4718e96bed282cc20ba4d38baf4583314c61e7be
amir-du <amir.dulas@supdevinci-edu.fr>
2026-06-24 12:52:29 +0200

    v0.1 - test connexion SSH sur equipement Cisco GNS3

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from netmiko import ConnectHandler
 
+# Connexion à un équipement Cisco via SSH
 cisco_01 = {
-    "device_type": "cisco_ios",
-    "host": "10.10.201.9",
-    "username": "adminocp",
-    "password": "ocp1920"
+    "device_type": "cisco_ios_telnet",
+    "host": "127.0.0.1",
+    "port": 5000,
+    "username": "admin",
+    "password": "cisco",
+    "secret": "cisco"
 }
 
 connection = ConnectHandler(**cisco_01)
+connection.enable()
 output = connection.send_command("show run | inc hostname")
 print(output)
 connection.disconnect()
